07bfe14879330fb3f0dfed55a0c3d9dca58cc270
Thibault <me@tbmc.fr>
2016-07-15 02:38:24 +0200

    Gestion des arguments avec argparse

--- FolderExtractor.py
+++ FolderExtractor.py
@@ -1,7 +1,7 @@
 import os
-import sys
 import shutil
 import re
+import argparse
 
 
 def extract_folder(dossier, base_dir=os.getcwd(), recursive=False, ext=False, regex=False):
@@ -10,6 +10,8 @@ def extract_folder(dossier, base_dir=os.getcwd(), recursive=False, ext=False, re
     :param dossier: Dossier dans lequel chercher les fichiers
     :param base_dir: Dossier dans lequel déplacer les fichiers trouvés
     :param recursive: Chercher dans tous les sous dossier et pas uniquement dans le dossier courrant
+    :param ext:
+    :param regex:
     """
     reg = False
 
@@ -34,69 +36,37 @@ def remove_sub_folder(dossier):
             shutil.rmtree(a)
 
 
-def print_help():
-    a = """
-Voici les quelques commandes utiles :
-Recursive : Extrait dans le dossier courant, tous les dossiers ainsi que tous les sous-dossiers
-RemoveFolders : Supprime les dossiers après avoir déplacé les fichiers (même si il reste encore des fichiers)
-Only regex "regex" : Déplace uniquement les fichiers correspondants à une regex
-Only ext "ext" : Déplace uniquement les fichiers correspondants à une certaine extension
-    """
-    print(a)
-
-arguments = {
-    "recursive": False,
-    "removefolders": False,
-    "regex": False,
-    "extension": False,
-}
-
-
-def arguments_helper():
-    l = len(sys.argv)
-    if l > 1:
-        i = 1
-        while i < l:
-            arg = sys.argv[i]
-            arg = arg.lower()
-            if arg == "recursive":
-                arguments["recursive"] = True
-            elif arg == "only":
-                arg1 = sys.argv[i+1].lower()
-                arg2 = sys.argv[i+2].lower()
-                i += 2
-                if i + 2 >= l:
-                    return "Il manque des arguments avec l'option 'only'"
-                if arg1 == "regex":
-                    arguments["regex"] = arg2
-                elif arg1 == "ext":
-                    arguments["extension"] = arg2
-                else:
-                    return "L'argument après 'only' est une action non gérée : {}".format(arg1)
-            elif arg == "removefolders":
-                arguments["removefolders"] = True
-            else:
-                return "Argument inconnu : {}".format(arg)
-            i += 1
-    return False
-
+def get_parser():
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("-R", "--recursive", action="store_true",
+                        help="Extrait depuis le dossier courant ainsi que tous \
+                        les sous dossiers")
+    argument_parser.add_argument("-rm", "--remove", action="store_true",
+                        help="Supprime les sous dossiers après en avoir extrait \
+                                 les fichiers")
+    argument_parser.add_argument("-x", "--regex", type=str,
+                        help="Déplace uniquement les fichiers correspondants \
+                            à la regex")
+    argument_parser.add_argument("-e", "--extension", type=str,
+                        help="Déplace uniquement les fichiers ayant l'extension \
+                                 passée en paramètre")
+    return argument_parser
 
 if __name__ == "__main__":
     print("Extract Folder")
-    error_arg = arguments_helper()
-    if error_arg:
-        print(error_arg)
-        print()
-        print_help()
-    else:
-        base = os.getcwd()
-        base = "B:\\BigBangTheoryS07"
-        print("En cours...")
-        extract_folder(base, base, arguments["recursive"],
-                       arguments["extension"], arguments["regex"])
-        if arguments["removefolders"]:
-            remove_sub_folder(base)
-        print("Fini")
+    parser = get_parser()
+    args = parser.parse_args()
+    print(args)
+
+    print("Déplacelement en cours...")
+
+    base = os.getcwd()
+    extract_folder(base, base, recursive=args.recursive, ext=args.extension, regex=args.regex)
+
+    if args.remove:
+        remove_sub_folder(base)
+
+    print("Fini")
 
 
 
